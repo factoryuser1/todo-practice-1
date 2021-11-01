ebb0d55dc34a0455d00422ce7e8ea67c3a4e1684
factoryuser1 <factoryuser1@yahoo.com>
2021-11-01 09:11:09 -0500

    update proxy

--- src/App.js
+++ src/App.js
@@ -6,14 +6,15 @@ import Items from "./components/Items";
 
 function App(){
     let [items, setItems] = useState([])
+    let url ="http://localhost:3001/api/items";
 
     const getAllItems = () =>{
-        axios.get("http://localhost:3001/api/items")
+        axios.get("/api/items")
             .then(result =>{
                 if(result.status === 200) {
-                    console.log('good')
+                    console.log('SUCCESS')
                 } else {
-                    console.log('you messed up');
+                    console.log('FAILURE');
                 }
                 setItems(result.data)
                 console.log(result.statusText);
@@ -32,29 +33,29 @@ function App(){
     }, []); //only if it is empty
 
     const handleCompletedItemChecked = async (item) =>{
-        await axios.patch(`http://localhost:3001/api/items/${item.id}`, {completed:!item.completed});
+        await axios.patch(`/api/items/${item.id}`, {completed:!item.completed});
         getAllItems();
     }
     const handleDeleteItem = async (item) =>{
-        await axios.delete(`http://localhost:3001/api/items/${item.id}`);
+        await axios.delete(`/api/items/${item.id}`);
         getAllItems();
     }
 
     const editTodoItem = async (item) => {
         console.log("inside editToDo")
-        await axios.patch(`http://localhost:3001/api/items/${item.id}`, {content: item.content});
+        await axios.patch(`/api/items/${item.id}`, {content: item.content});
         getAllItems();
     }
 
     const createTodoItem = async (item) => {
         console.log("inside createToDo")
-        await axios.post("http://localhost:3001/api/items", item);
+        await axios.post("/api/items", item);
         getAllItems();
     }
 
-    function displayUpdateItem() {
-
-    }
+    // function displayUpdateItem() {
+    //
+    // }
     return(
         <div>
             <span><h3>Your Too Items List</h3></span>
@@ -66,7 +67,8 @@ function App(){
             <TodoCreator
                 items={items}
                 createTodoItem={createTodoItem}/>
-            {displayUpdateItem()}
+            {/*{displayUpdateItem()}*/}
+
         </div>
     )
 }
